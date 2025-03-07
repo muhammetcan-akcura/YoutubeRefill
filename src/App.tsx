183bce5f9847cfa74463aa954c246751d32ad1ec
unknown <akcuramuhammet658@gmail.com>
2025-03-07 14:16:03 +0300

    puhs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import YoutubeViewerCheck from './components/YoutubeViewerCheck';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800">
+      <YoutubeViewerCheck />
+    </div>
+  );
+}
+
+export default App;
